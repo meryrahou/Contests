3d5c607e8717267173619cb826921c766ba7b338
RAHOU Meriem <lm_rahou@esi.dz>
2024-07-23 16:26:34 +0100

    Fixed one

--- D_Colorful_Table.py
+++ D_Colorful_Table.py
@@ -4,9 +4,3 @@ for _ in range(t):
     n, k = map(int, input().split())
 
     a = list(map(int, input().split()))
-
-    for i in range(k):
-        # go through b matrix and findout 
-        # if i color is in half of the border of b matrix
-
-        
